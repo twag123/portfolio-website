a6e0a441667aa434c521e75c378396645d2b300c
twag123 <tylerawagner123@gmail.com>
2023-07-07 15:27:07 -0500

    Linking to sections
    - Added working links to the different sections of the site
    - Middle sections are offset by +100 padding and -100 margin to offset for the opaque static header

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,11 @@
 // Change styling of nav bar active button when pressed
-$(".nav-link").on("click", function () {
+$(".nav-link").on('click', function () {
     $(".nav-link").removeClass("active");
     $(this).toggleClass("active");
+});
+
+
+// Scroll to the top of page when clicked
+$("#home").on('click', function () {
+    $('html.body').animate({ scrollTop: 0 }, 'fast');
 });
